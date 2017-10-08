6f6621a89b5c85bd43563e334157f581d7b7e924
Masterflash <sxq@yandex.ru>
2017-10-08 19:32:28 +0300

    add locale

--- src/Lib/Func/GetMvc.php
+++ src/Lib/Func/GetMvc.php
@@ -1,27 +1,27 @@
 <?php
 namespace Menu\Lib\Func;
-$GLOBALS['for_menu_admin_interface_class']=false;
 
 
 class GetMvc
 {
 
+public static $cache;
 
 public function __invoke ($obj,$infa,$struct_arr,$pole_type,$pole_dop,$tab_name,$idname,$const,$id,$action,$i__,$j__)
 {
 
 
-if (!$GLOBALS['for_menu_admin_interface_class']) 
-{
+if (!isset(self::$cache[$obj->pole_dop[0]]) )
+{	//кеш
+		self::$cache[$obj->pole_dop[0]]=$obj->EventManager->trigger("GetControllersInfoAdmin",NULL,["locale"=>$obj->pole_dop[0],"name"=>"","container"=>$obj->container]);
+}
 	$obj->sp['sql'][$j__]['sp_group_array']=[];
 	$obj->sp['sql'][$j__]['name']=[];
 	$obj->sp['sql'][$j__]['id']=[];
 	
-	
-	$controllers_description=$obj->EventManager->trigger("GetControllersInfoAdmin",NULL,["name"=>"","container"=>$obj->container]);
 	//цикл по контроллерам
 	//конвертируем в старый формат
-	foreach ($controllers_description as $name=>$desc)
+	foreach (self::$cache[$obj->pole_dop[0]] as $name=>$desc)
 		{
 			//внутри контроллера
 			if (is_array($desc))
@@ -31,14 +31,11 @@ if (!$GLOBALS['for_menu_admin_interface_class'])
 							$obj->sp['sql'][$j__]['sp_group_array'][]=$meta["description"];
 							$obj->sp['sql'][$j__]['name'][]=$meta["urls"]['name'];		
 							$obj->sp['sql'][$j__]['id'][]=$meta["urls"]['mvc'];
-							//
 						}
 				}
 		}
 	
-}
 
-$GLOBALS['for_menu_admin_interface_class']=true;
 return $infa;
 }
 
